2f244dde39d022c0674f85ff974e963bef85c3b7
ESZ-98 <emilia.zalewska.gdn@gmail.com>
2023-03-20 19:06:35 +0000

    Deploying to gh-pages from @ ESZ-98/goit-js-hw-09@64831ebfea12399f521cf061f176cadbd8e5ddb9 🚀

---
+++ 01-color-switcher.4f06f025.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d=null;e.setAttribute("disabled",!0),t.addEventListener("click",(()=>{t.setAttribute("disabled",!0),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{e.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.4f06f025.js.map
